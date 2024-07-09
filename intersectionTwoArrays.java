49a96df14b21f8adf28ef254f31f138316dcfd66
Pranay Reddy <pranay.edula@gmail.com>
2024-07-08 23:08:52 -0500

    Intersect 2 arrays completed

---
+++ intersectionTwoArrays.java
@@ -0,0 +1,34 @@
+/*
+ * TIme complexity - O(mxn)
+ * Space complexity - O(1) constant
+ */
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        if(m<n){
+            intersect(nums2, nums1);
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+
+        for(int i = 0; i< n; i++){
+            map.put(nums2[i], map.getOrDefault(nums2[i], 0)+1);
+        }
+        for(int i = 0; i < m; i++){
+            if(map.containsKey(nums1[i])){
+                result.add(nums1[i]);
+                int count = map.get(nums1[i]);
+                count--;
+                map.put(nums1[i], count);
+                map.remove(nums1[i], 0);
+            }
+        }
+        int[] finalResult = new int[result.size()];
+        for(int i = 0; i < result.size(); i++){
+            finalResult[i] = result.get(i);
+        }
+        return finalResult;
+    }
+}
